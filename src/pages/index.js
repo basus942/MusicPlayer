75d2554f0bd235ee4bcff79c525d3b3887a925d1
Subhajit basu <basus942@gmail.com>
2023-09-22 23:36:30 +0530

    added padding on top for web view

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import MusicPlayer from "@/components/MusicPlayer";
 export default function Home() {
   return (
     <Layout>
-      <div className="flex justify-center items-center m-2">
+      <div className="flex justify-center items-center m-2 pt-10">
         <MusicPlayer />
       </div>
     </Layout>
