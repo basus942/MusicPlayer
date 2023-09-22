fe10d5b2193e82e62bdf3e67e2923985cb62af21
Subhajit basu <basus942@gmail.com>
2023-09-22 23:47:00 +0530

    padding top

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import MusicPlayer from "@/components/MusicPlayer";
 export default function Home() {
   return (
     <Layout>
-      <div className="flex justify-center items-center m-2 md:pt-[10rem]">
+      <div className="flex justify-center items-center m-2 sm:pt-[10rem]">
         <MusicPlayer />
       </div>
     </Layout>
